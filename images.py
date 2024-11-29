73f7658d990a022db0320a29e711c56346f99485
Carlos <chlorenzo96@gmail.com>
2024-11-29 22:43:53 +0100

    initial commit

---
+++ images.py
@@ -0,0 +1,41 @@
+import os
+import re
+import shutil
+
+# Paths
+posts_dir = "/home/chl/Documents/pagina_web/Blog/blog/content/Pagina_Web/Blog/Posts/"
+attachments_dir = "/home/chl/Documents/Obsidian Vault/Pagina_Web/images/"
+static_images_dir = "/home/chl/Documents/pagina_web/Blog/blog/static/images/"
+
+#test
+images = os.listdir(attachments_dir)
+print(f"Imágenes en el directorio de origen: {images}")
+
+
+# Step 1: Process each markdown file in the posts directory
+for filename in os.listdir(posts_dir):
+    if filename.endswith(".md"):
+        filepath = os.path.join(posts_dir, filename)
+        
+        with open(filepath, "r") as file:
+            content = file.read()
+        
+        # Step 2: Find all image links in the format ![Image Description](/images/Pasted%20image%20...%20.png)
+        images = re.findall(r'\[\[([^]]*\.png)\]\]', content)
+        
+        # Step 3: Replace image links and ensure URLs are correctly formatted
+        for image in images:
+            # Prepare the Markdown-compatible link with %20 replacing spaces
+            markdown_image = f"![Image Description](../../images/{image.replace(' ', '%20')})"
+            content = content.replace(f"[[{image}]]", markdown_image)
+                        
+            # Step 4: Copy the image to the Hugo static/images directory if it exists
+            image_source = os.path.join(attachments_dir, image)
+            if os.path.exists(image_source):
+                shutil.copy(image_source, static_images_dir)
+
+        # Step 5: Write the updated content back to the markdown file
+        with open(filepath, "w") as file:
+            file.write(content)
+
+print("Markdown files processed and images copied successfully.")
