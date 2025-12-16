73bda1df06eeaa4053eb0560b3661fa265265098
Carlos Hernández Lorenzo <chlorenzo96@gmail.com>
2025-12-16 23:07:39 +0000

    pre subida imagenes al post de whatsapp

---
+++ src/pages/Blog/seguridad-whatsapp.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+export default function SeguridadWhatsapp() {
+  return (
+    <div>
+      //<img src="/images/Blog/seguridad_whatsapp/instagram.png" alt="test" />
+      <br />
+      <p>En la era digital actual, nuestras aplicaciones de mensajería y redes sociales son una parte esencial de nuestras vidas. Como WhatsApp, Instagram, Telegram y TikTok, millones de personas comparten sus mensajes, fotos, vídeos y pensamientos cada día. Sin embargo, muchos de nosotros no somos conscientes de los riesgos de privacidad que conllevan estas plataformas.</p>
+      <br />
+      <p>En este post, vamos a mejorar la seguridad en una de las aplicaciones más populares y cómo puedes proteger tu información personal sin complicaciones.</p>
+      <br />
+      <p>¿Qué sabe WhatsApp sobre ti? Aunque WhatsApp usa cifrado de extremo a extremo, eso no significa que sea completamente privado. Aparte del contenido del mensaje, WhatsApp recopila:</p>
+      <br />
+      <p>Tu número de teléfono y contactos</p>
+      <p>Información de uso (con qué frecuencia usas la app, a quién contactas, etc.)</p>
+      <p>Tu dirección IP, modelo de móvil, y datos del sistema</p>
+      <p>Ubicación (si compartes o das permiso)</p>
+      <p>Metadatos (quién envía mensajes, cuándo, desde dónde)</p>
+      <br />
+      <p>Y recordemos: WhatsApp es propiedad de Meta (Facebook), y algunos de esos datos pueden usarse para personalizar anuncios en otras plataformas.</p>
+      <br />
+      <h2>Configuraciones clave para mejorar tu privacidad</h2>
+      <img src="/images/Blog/seguridad_instagram/instagram1.png" alt="test" />
+      <br />
+      <ul>Abre Instagram → Más → Configuración → Contraseña y seguridad</ul>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram2.png" alt="test" />
+      <br />
+      <p>Seleccionamos Contraseña y seguridad → Cambiar contraseña</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram3.png" alt="test" />
+      <br />
+      <p>2. Activa la autenticación en 2 pasos</p>
+      <br />
+      <p>Seleccionamos Contraseña y seguridad → Autenticación en dos pasos</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram4.png" alt="test" />
+      <br />
+      <p>A continuación nos va a pedir por donde vamos a configurar ese 2do factor de autenticación, ya sea por sms, whatsapp o alguna aplicación tipo one time token.</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram5.png" alt="test" />
+      <br />
+      <p>Como vemos en la propia aplicación nos recomiendan hacerlo a través de una aplicación, yo personalmente uso Microsoft autenticator, pero puedes escoger la que mejor te encaje.</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram6.png" alt="test" />
+      <br />
+      <p>Luego que nos va aparecer un código similar a este para que desde nuestra aplicación lo escaneemos.</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram7.png" alt="test" />
+      <br />
+      <p>A continuación la aplicación del móvil nos va a generar un numero de 6 dígitos que vamos a introducir en Instagram validando esta segunda capa de seguridad sobre nuestra cuenta.</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram8.png" alt="test" />
+      <br />
+      <p>3. Revisar Controles de seguridad.</p>
+      <br />
+      <p>Seleccionamos Contraseña y seguridad → Controles de seguridad</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram9.png" alt="test" />
+      <br />
+      <p>En esta sección debemos verificar que los dispositivos que hayan iniciado sesión sean solamente los nuestros, eliminar las sesiones iniciadas en los dispositivos que no vayamos a usar mas, dígase teléfono o pc de alguna persona cercana. Revisemos los correos electrónicos asociados a la cuenta sean solamente los nuestros.</p>
+      <br />
+      <p>4. dministrar cookies.</p>
+      <br />
+      <p>Seleccionamos en el panel de seguridad Tu información y permisos → Administrar cookies.</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram10.png" alt="test" />
+      <br />
+      <p>Recomendación deshabilitar todas las cookies posibles, solamente permitir las imprescindibles para el funcionamiento de la aplicación.</p>
+      <br />
+      <img src="/images/Blog/seguridad_instagram/instagram11.png" alt="test" />
+      <br />
+      <br />
+      <p>Buenas prácticas de seguridad en Instagram.</p>
+      <br />
+      <p>Activa la autenticación en dos pasos con una app segura (evita usar SMS si es posible).
+         Revisa con frecuencia los dispositivos donde tienes la sesión iniciada.
+         No hagas clic en enlaces sospechosos en mensajes directos (DMs), aunque parezcan venir de conocidos.
+         No compartas códigos de verificación. Instagram nunca te los pedirá por mensaje ni correo.
+      </p>
+      <br />
+      <p>¿Y tú? ¿Ya habías revisado estas configuraciones en tu cuenta?</p>
+      <br />
+      <p>Proteger tu cuenta de Instagram es más fácil de lo que parece, y puede evitarte muchos dolores de cabeza.</p>
+      <br />
+      <p>Dedica unos minutos hoy y navega con más tranquilidad mañana.</p>
+      <br />
+      <p>Si te resultó útil, ¡comparte este post con quien también debería reforzar su seguridad!</p>
+      <br />
+      </div>
+  );
+}
