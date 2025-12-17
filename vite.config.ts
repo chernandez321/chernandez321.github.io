163eae58923956a5e5e67eb1eeb9b427aae391d6
Carlos Hernández Lorenzo <chlorenzo96@gmail.com>
2025-12-17 21:55:25 +0000

    arreglando el refresh

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./",   
+  base: "/",
   server: {
     host: "::",
     port: 8080,
